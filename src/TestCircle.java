f81e3b47662163db12591e2cc886addf1457e48c
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-10-13 15:00:51 +0700

    lop circle va lop cylinder

---
+++ src/TestCircle.java
@@ -0,0 +1,8 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(2.4, "purple");
+
+        System.out.println(c1.toString());
+        System.out.println(c1.getArea());
+    }
+}
---
+++ src/TestCylinder.java
@@ -0,0 +1,14 @@
+public class TestCylinder {
+    public static void main(String[] args) {
+//        Circle cy1;
+//        cy1 = new Cylinder(2.3, "white",3.0);
+//        Cylinder c2 = (Cylinder) cy1;
+
+        Cylinder cy1 = new Cylinder(2.3, "white",3.0);
+        System.out.println(cy1);
+        System.out.println(cy1.getVolume());
+//        System.out.println(c2.getVolume());
+        System.out.println(cy1.getArea());
+
+    }
+}
